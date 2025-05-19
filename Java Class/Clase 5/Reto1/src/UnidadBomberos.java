5215270791111215f47fcea4587d661ae88c3dfc
Diana <tr447113@uaeh.edu.mx>
2025-05-19 13:43:38 -0600

    Class 5
    Reto 1 y 2 de la clase 5

---
+++ Java Class/Clase 5/Reto1/src/UnidadBomberos.java
@@ -0,0 +1,25 @@
+public class UnidadBomberos extends UnidadEmergencia {
+    private SistemaGPS gps;
+    private Sirena sirena;
+    private Solucion_Reto_01.Operador operador;
+
+    public UnidadBomberos(String nombre, String nombreOperador) {
+        super(nombre);
+        this.gps = new SistemaGPS();
+        this.sirena = new Sirena();
+        this.operador = new Solucion_Reto_01.Operador(nombreOperador);
+    }
+
+    public void iniciarOperacion() {
+        activarUnidad();
+        gps.localizar();
+        sirena.activarSirena();
+        operador.reportarse();
+        responder();
+    }
+
+    @Override
+    public void responder() {
+        System.out.println("🔥 Unidad de bomberos respondiendo a incendio estructural.");
+    }
+}
